e7a80486f32687ee9cd3ca173448d12d557bfa88
polinalester <epikh1gh@yandex.ru>
2018-04-22 03:01:29 +0300

    Update render service

--- RenderService/RenderService.cs
+++ RenderService/RenderService.cs
@@ -99,20 +99,50 @@ namespace RenderService
             });
         }
 
-        long counter;
+        //long counter;
 
         public Response OnRequest(Request r) {
             var reqtype = r.Type;
-            //switch(r.Type) {
-              //  case RenderRequest:
-                    //var mr = (RenderRequest)r;
+            switch(r.Type) {
+                case RenderRequest:
+                    var mr = (RenderRequest)r;
 
                     Console.WriteLine("Done");
+                    string baseName = "C:/Users/polina/Downloads/Quadrario/Quadrario/playerdb.db3";
+            List<int[]> players = new List<int[]>();
 
-                    return new BooleanResponse { Ok = true };
-               // default:
-                //    throw new ArgumentOutOfRangeException();
-            //}
+            SQLiteConnection cnnect = new SQLiteConnection("Data Source=" + baseName + ";Version=3;");
+                    cnnect.Open();
+                    SQLiteCommand command = new SQLiteCommand(); ;
+                    command.Connection = cnnect;
+                    command.CommandText = @"SELECT * FROM";
+                    Console.WriteLine("Render request from {0}", mr.UserId);
+                    command.CommandType = CommandType.Text;
+                    command.ExecuteNonQuery();
+            try
+            {
+                SQLiteDataReader reader = command.ExecuteReader();
+                string line = String.Empty;
+                while (reader.Read())
+                {
+                    int[] playerInfo = new int[4];
+                    playerInfo[0] = Convert.ToInt32(reader["id"]);
+                    playerInfo[1] = Convert.ToInt32(reader["xcoordinate"]);
+                    playerInfo[2] = Convert.ToInt32(reader["ycoordinate"]);
+                    playerInfo[3] = Convert.ToInt32(reader["size"]);
+                    players.Add(playerInfo);
+                }
+                reader.Close();
+            }
+            catch (SQLiteException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            cnnect.Close();
+            return new RenderResponse { Players = players };
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
         }
     }
 }
