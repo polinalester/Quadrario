941c8f0b850cb0a5d051c46c45d042f17c82fedc
polinalester <epikh1gh@yandex.ru>
2018-04-22 03:03:04 +0300

    Update intersection service

--- IntersectionService/IntersectionService.cs
+++ IntersectionService/IntersectionService.cs
@@ -32,7 +32,7 @@ namespace IntersectionService
         public void CallDirectoryService()
         {
             ThreadPool.QueueUserWorkItem(state => {
-                var req = new ServiceHostRequest() { Address = _ip, ServiceType = "move" };
+                var req = new ServiceHostRequest() { Address = _ip, ServiceType = "intersect" };
                 var dirServiceSocket = new RequestSocket();
                 dirServiceSocket.Connect("tcp://" + _dirSerIp + ":8910");
                 while (!_stopLoops)
@@ -71,7 +71,7 @@ namespace IntersectionService
         {
             ThreadPool.QueueUserWorkItem(state => {
                 var server = new PublisherSocket();
-                server.Bind("tcp://*:5556");
+                server.Bind("tcp://*:5554");
                 while (!_stopLoops)
                 {
                     try
@@ -97,7 +97,7 @@ namespace IntersectionService
             //TODO: move into server app!
             ThreadPool.QueueUserWorkItem(state => {
                 var server = new ResponseSocket();
-                server.Bind("tcp://*:5555");
+                server.Bind("tcp://*:5553");
                 while (!_stopLoops)
                 {
                     try
@@ -125,8 +125,8 @@ namespace IntersectionService
         {
             switch (r.Type)
             {
-                case MessageTypes.MoveRequest:
-                    var mr = (MoveRequest)r;
+                case MessageTypes.IntersectRequest:
+                    var ir = (IntersectRequest)r;
 
                     string baseName = "C:/Users/polina/Downloads/Quadrario/Quadrario/playerdb.db3";
 
@@ -135,35 +135,63 @@ namespace IntersectionService
                     SQLiteCommand command = new SQLiteCommand(); ;
                     command.Connection = cnnect;
 
-                    Console.WriteLine("Move request from {0}", mr.UserId);
-
-                    if (mr.Direction[1] == 1)
-                    {
-                        Console.WriteLine("move right");
-                        command.CommandText = @"UPDATE [players] SET [xcoordinate] = [xcoordinate] + 5 "
-                            + "WHERE [id] = " + mr.UserId;
-                    }
-                    else if (mr.Direction[1] == -1)
-                    {
-                        Console.WriteLine("move left");
-                        command.CommandText = @"UPDATE [players] SET [xcoordinate] = [xcoordinate] - 5 "
-                            + "WHERE [id] = " + mr.UserId;
-                    }
-                    else if (mr.Direction[2] == 1)
-                    {
-                        Console.WriteLine("move up");
-                        command.CommandText = @"UPDATE [players] SET [ycoordinate] = [ycoordinate] - 5 "
-                            + "WHERE [id] = " + mr.UserId;
-                    }
-                    else //(mr.Direction[2] == -1)
-                    {
-                        Console.WriteLine("move down");
-                        command.CommandText = @"UPDATE [players] SET [ycoordinate] = [ycoordinate] + 5 "
-                            + "WHERE [id] = " + mr.UserId;
-                    }
+                    Console.WriteLine("Intersect request from {0}", ir.UserId);
+                    int xpos = 0, ypos = 0, size = 0;
+                    command.CommandText = @"SELECT * FROM [players] WHERE [id] = " + ir.UserId;
                     command.CommandType = CommandType.Text;
                     command.ExecuteNonQuery();
+                    try
+                    {
+                        SQLiteDataReader reader = command.ExecuteReader();
+                        string line = String.Empty;
+                        while (reader.Read())
+                        {
+                            xpos = Convert.ToInt32(reader["xcoordinate"]);
+                            ypos = Convert.ToInt32(reader["ycoordinate"]);
+                            size = Convert.ToInt32(reader["size"]);
+                        }
+                        reader.Close();
+                    }
+                    catch (SQLiteException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    string intersected = "(";
+                    command.CommandText = @"SELECT * FROM [players] WHERE [id] != " + ir.UserId
+                        + " AND (" + (xpos + size) + " >= [xcoordinate] OR [xcoordinate] + [size] >= "
+                        + xpos + " OR [ycoordinate]+[size] >= " + ypos + " OR " + (ypos + size) + " >= [ycoordinate]";
+                    command.CommandType = CommandType.Text;
+                    command.ExecuteNonQuery();
+                    try
+                    {
+                        SQLiteDataReader reader = command.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            intersected += reader["id"];
+                            intersected += ", ";
+                        }
+                        if (intersected.Length > 2)
+                        {
+                            intersected = intersected.Substring(0, intersected.Length - 3);  
+                        }   
+                        reader.Close();
+                    }
+                    catch (SQLiteException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    intersected += ")";
+                    if (intersected.Length > 2)
+                    {
+                        command.CommandText = @"UPDATE [players] SET [size] = [size] + 10 WHERE [id] = " + ir.UserId;
+                        command.CommandType = CommandType.Text;
+                        command.ExecuteNonQuery();
+                        command.CommandText = @"UPDATE [players] SET [size] = [size] - 10 WHERE [id] IN (" + intersected;
+                        command.CommandType = CommandType.Text;
+                        command.ExecuteNonQuery();
 
+                    }
+                    cnnect.Close();
                     return new BooleanResponse { Ok = true };
                 default:
                     throw new ArgumentOutOfRangeException();
